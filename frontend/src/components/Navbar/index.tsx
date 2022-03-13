3c26baa9698a5f16b02024439e3fd302bb42b5be
Philipe Gomes <philipee.barbosa@gmail.com>
2022-03-13 12:48:33 -0300

    Form layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,23 +1,23 @@
-import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css'
 
-function Navbar(){
+function Navbar() {
 
-    return (
-      <header>
-        <nav className="container">
-          <div className="dsmovie-nav-content">
-            <h1>DSMovie</h1>
-            <a href="https://github.com/PhilipeGomes">
-              <div className="dsmovie-contact-container">
-                <GithubIcon />
-                <p className="dsmovie-contact-link">/PhilipeGomes</p>
-              </div>
-            </a>
-          </div>
-        </nav>
-      </header>
-    );
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/PhilipeGomes">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/PhilipeGomes</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
